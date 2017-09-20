26386d5c151640c67531697d04ee65ceaf2a24d1
a544jh <axel.wikstrom@gmail.com>
2017-09-21 00:04:07 +0300

    somewhat working xor now

--- receiver.py
+++ receiver.py
@@ -2,6 +2,7 @@ import socket
 
 IP = "127.0.0.1"
 PORT = 5005
+USE_XOR = True
 
 def decodePacket(packet):
   seqno_bytes = packet[0:8]
@@ -10,26 +11,66 @@ def decodePacket(packet):
   payload = packet[9:]
   return seqno, end_flag_byte, payload
 
+def xorBytes(a, b):
+  c = bytearray(len(a))
+  for i in range(len(a)):
+    c[i] = a[i] ^ b[i]
+  return c
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((IP, PORT))
 
-receivedData = []
+receivedData = bytearray()
 expectedSeqno = 0
+bufferA = bytearray()
+bufferB = bytearray()
 while True:
   packet, addr = sock.recvfrom(1024)
   seqno, end_flag_byte, payload = decodePacket(packet)
-  #print(seqno, end='')
-  #if end_flag_byte == 1:
-  #  print(' END', end='')
-  #print()
-  #print(''.join('{:02X} '.format(b) for b in payload))
-  if seqno == expectedSeqno:
-    receivedData.extend(payload)
-    if end_flag_byte == 1:
-      break
-    expectedSeqno += 1
-  #else:
-  #  print("Expected packet {}, but got {}".format(expectedSeqno, seqno))
+  if not USE_XOR:
+    if seqno == expectedSeqno:
+      receivedData.extend(payload)
+      expectedSeqno += 1
+      if end_flag_byte == 1:
+        break
+  else: 
+    packetType = seqno % 3
+    withinWindow = expectedSeqno + packetType == seqno
+    if not withinWindow:
+      continue
+    if packetType == 0: #a
+      print("Receved A packet " + str(seqno))
+      # last packet is A, end here
+      if end_flag_byte == 1:
+        receivedData.extend(payload)
+        break
+      bufferA.extend(payload)
+    elif packetType == 1: #b
+      print("Receved B packet " + str(seqno))
+      bufferB.extend(payload)
+    elif packetType == 2: #c
+      print("Receved C packet " + str(seqno))
+      if len(bufferA) == 0:
+        print("A = B xor C")
+        bufferA.extend(xorBytes(bufferB, payload))
+      elif len(bufferB) == 0:
+        print("B = A xor C")
+        bufferB.extend(xorBytes(bufferA, payload))
+    if len(bufferA) > 0 and len(bufferB) > 0:
+      receivedData.extend(bufferA)
+      receivedData.extend(bufferB)
+      bufferA.clear()
+      bufferB.clear()
+      # move on to next window
+      expectedSeqno += 3
+      if end_flag_byte == 1:
+        break
+
+  
 
 print("Received whole file! {} bytes".format(len(receivedData)))
 #print(bytes(receivedData))
+
+file = open('out', 'bw')
+file.write(receivedData)
+file.close()
--- sender.py
+++ sender.py
@@ -5,31 +5,47 @@ import time
 
 IP = "127.0.0.1"
 PORT = 5005
-DROP_CHANCE = 0
-USE_XOR = False
+DROP_CHANCE = 0.01
+USE_XOR = True
 
 def encodeHeader(seqno, end_flag):
   seqno_bytes = seqno.to_bytes(8,'little')
   end_flag_byte = end_flag.to_bytes(1,'little')
   return seqno_bytes + end_flag_byte
 
-def xorBytes(a, b, c):
+#TODO: fix EOF in C packet eg. by encoding length of B packet
+
+def xorBytes(a, b):
+  c = bytearray(len(a))
   for i in range(len(a)):
     c[i] = a[i] ^ b[i]
   return c
+
+def isXorPacket(seqno):
+  return seqno % 3 == 2
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 end_flag = 0
 data = sys.stdin.buffer.read()
 
+dataIndex = 0
 seqno = 0
-while end_flag != 1:
-  payload = data[seqno*100:(seqno*100)+100]
+while end_flag != 1 or (end_flag == 1 and USE_XOR and seqno % 3 == 2): #also send last C packet
+  if not USE_XOR or (not isXorPacket(seqno)):
+    print("Sending data packet " + str(seqno))
+    payload = data[dataIndex*100:(dataIndex*100)+100]
+    dataIndex += 1
+  else:
+    print("Sending XOR packet " + str(seqno))
+    a = data[(dataIndex-2)*100:((dataIndex-2)*100)+100]
+    b = data[(dataIndex-1)*100:((dataIndex-1)*100)+100]
+    payload = xorBytes(a,b)
   if len(payload) < 100:
     end_flag = 1
   header = encodeHeader(seqno, end_flag)
   packet = header + payload
-  for i in range(0,3):
+  repeats = 3 if not USE_XOR or (USE_XOR and end_flag == 1 and seqno % 3 == 0) else 1 #edge case: send three packets if xor ends at A packet
+  for i in range(0,repeats):
     if random.random() >= DROP_CHANCE:
       sock.sendto(packet, (IP, PORT))
       #print("Sent packet {}".format(seqno))
